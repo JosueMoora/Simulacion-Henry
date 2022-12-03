e433e0074e949797d8794a5fd48b8f825c92b805
Josue Mora <josuedavid_99@hotmail.com>
2022-12-02 21:34:20 -0400

    01.js hecha

--- 01.js
+++ 01.js
@@ -11,7 +11,9 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  const soloEnteros = array.filter(numero => Number.isInteger(numero));
 
+  return soloEnteros;
 }
 
 // No modifiques nada debajo de esta linea //
